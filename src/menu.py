7622b0c2c0b9691bc17daa5e94b874911e9706a0
Tams <t.bolanos.1@estudiantec.cr>
2024-04-09 15:04:05 -0600

    Inputs for ask for columns, rows, and nicknames. - Asignation - Validation - Error Handlers

--- src/menu.py
+++ src/menu.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-from game_data import players
+from game_data import players, board_colums, board_rows
+from tkinter import messagebox
 import tk_widgets.custom_tk_widgets as custom
 
 window_menu = tk.Tk()
@@ -16,13 +17,54 @@ def register_players(name: str):
     }
 
     players.append(new_player)
-    print(players)
 
-def register_players_and_close_window(window, player_name1: tk.Entry, player_name2: tk.Entry):
+def set_game_config(window, player_name1: tk.Entry, player_name2: tk.Entry, rows_box: tk.Entry, columns_box: tk.Entry):
+    global board_colums
+    global board_rows
+    
     name1 = player_name1.get()
     name2 = player_name2.get()
+    
+    if name1 == "" or name2 == "":
+        tk.messagebox.showinfo("Error", "Debe registrar los jugadores.")
+        window.mainloop()
+    elif name1 == name2:
+        tk.messagebox.showinfo("Error", "Los nombres de los jugadores deben ser diferentes.")
+        window.mainloop()
+   
+    try:
+        rows = int(rows_box.get())
+        
+        if  rows < 10:
+            tk.messagebox.showinfo("Error", "El número de filas debe ser mínimo 10.")
+            window.mainloop()
+
+        else:
+            global board_rows 
+            board_rows = rows
+            
+    except ValueError:
+        tk.messagebox.showinfo("Error", "El número de filas debe ser un número entero positivo.")
+        window.mainloop()
+    try:
+        columns = int(columns_box.get())
+        
+        if  columns < 20:
+            tk.messagebox.showinfo("Error", "El número de columnas debe ser mínimo 20.")
+            window.mainloop()
+        elif columns % 2 != 0:
+            tk.messagebox.showinfo("Error", "El número de columnas debe ser PAR.")
+            window.mainloop()
+        else:
+            global board_colums 
+            board_colums = columns
+    except ValueError:
+         tk.messagebox.showinfo("Error", "El número de columnas debe ser un número entero positivo.")
+         window.mainloop() 
+
     register_players(name1)
     register_players(name2)
+    
     window.destroy()
 
 def player_registration():
@@ -31,24 +73,43 @@ def player_registration():
     window_player_form = tk.Tk()
 
     window_player_form.title("Menú")
-    window_player_form.geometry("500x500")
+    window_player_form.geometry("580x450")
     window_player_form.resizable(0,0)
+    
+    game_log = custom.Label(window_player_form, "Registro de Jugadores", 17)
+    game_log.place(x=187, y=20)
+    
+    player1 = custom.Label(window_player_form, "Nombre del jugador 1:")
+    player1.place(x=43, y=85)
+    player_name1 = custom.Entry(window_player_form)
+    player_name1.place(x=40, y=131)
 
-    game_log = tk.Label(window_player_form, text="Registro de Jugadores", font=("Times New Roman", 20))
-    game_log.place(x=125, y=40)
+    player2 = custom.Label(window_player_form, "Nombre del jugador 2:")
+    player2.place(x=340, y=85)
+    player_name2 = custom.Entry(window_player_form)
+    player_name2.place(x=340, y=131)
+    
+    game_settings = custom.Label(window_player_form, "Configuración del Juego", 17)
+    game_settings.place(x=178, y=205)
+    
+    
+    rows = custom.Label(window_player_form, "Número de filas:")
+    rows.place(x = 43, y = 260)
+    rows_notice = custom.Label(window_player_form, "Min:10", 10)
+    rows_notice.place(x = 180, y = 287)
+    rows_box = custom.Entry(window_player_form)
+    rows_box.place(x=180, y=260, width= 50)
+    
+    columns = custom.Label(window_player_form, "Número de columnas:")
+    columns.place(x = 305, y = 260)
+    columns_notice = custom.Label(window_player_form, "Min:20", 10)
+    columns_notice.place(x = 485, y = 287)
+    columns_entry = custom.Entry(window_player_form)
+    columns_entry.place(x=485, y=260, width= 50)
+    
 
-    player1 = tk.Label(window_player_form, text="Coloque el nombre del jugador 1:", font=("Times New Roman", 15))
-    player1.place(x=10, y=125)
-    player_name1 = tk.Entry(window_player_form, font=("Times New Roman", 15))
-    player_name1.place(x=10, y=160)
-
-    player2 = tk.Label(window_player_form, text="Coloque el nombre del jugador 2:", font=("Times New Roman", 15))
-    player2.place(x=10, y=190)
-    player_name2 = tk.Entry(window_player_form, font=("Times New Roman", 15))
-    player_name2.place(x=10, y=220)
-
-    accept_button = custom.Button(window_player_form, "Continuar", lambda: register_players_and_close_window(window_player_form, player_name1, player_name2))
-    accept_button.place(x=200, y=260)
+    accept_button = custom.Button(window_player_form, "Continuar", lambda: set_game_config(window_player_form, player_name1, player_name2, rows_box, columns_entry))
+    accept_button.place(x=235, y=355)
 
     window_player_form.mainloop()
 
@@ -61,7 +122,9 @@ window_menu.resizable(0, 0) #Whit this the window size can´t change.
 #create labels.
 #In the parentheses we place the window to wich the label is linked and after comma the type of label.
 #"font" is to change the font and letter size.
-welcome = tk.Label(window_menu, text = "¡Bienvenidos a Battleship!", font = ("Times New Roman", 20)).place(x = 123, y = 135)
+welcome = custom.Label(window_menu,"¡Bienvenidos a Battleship!", 20)
+# welcome = tk.Label(window_menu, text = "¡Bienvenidos a Battleship!")
+welcome.place(x = 123, y = 135)
 # registration = tk.Label(window_menu, text = "¿Desean registrarse?", font = ("Times New Roman", 17)).place(x = 160, y = 185)
 
 #create button.
