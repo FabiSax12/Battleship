0685930294fad5f7721145d3a1b304d65bf8785a
Fabián Vargas <fabidev18@gmail.com>
2024-04-12 14:22:34 -0600

    Changing Custom Widgets from Functions to Classes

--- src/tk_widgets/custom_tk_widgets.py
+++ src/tk_widgets/custom_tk_widgets.py
@@ -1,45 +1,38 @@
 import tkinter as tk
 from enums import Color
 
-def Div(window: tk.Tk, *args, **kargs) -> tk.Frame:
-    return tk.Frame(
-        window,
-        *args, **kargs
-    )
+class Frame(tk.Frame):
+    def __init__(self, window: tk.Tk, *args, **kargs):
+        super().__init__(
+            window,
+            *args, **kargs
+        )
 
-# class miBoton(tk.Button):
-#     def __init__(self, window: tk.Tk, text: str = "", on_click = None, *args, **kargs):
-#         super().__init__(
-#             window, 
-#             text = text, 
-#             font = ("Times New Roman", 17), 
-#             bg = Color.BLACK.value, 
-#             fg = Color.WHITE.value,
-#             command=on_click,
-#             *args, **kargs
-#        )
-def Button(window: tk.Tk, text: str = "", on_click = None,  *args, **kargs) -> tk.Button:
-    return tk.Button(
-        window, 
-        text = text, 
-        font = ("Times New Roman", 17), 
-        bg = Color.BLACK.value, 
-        fg = Color.WHITE.value,
-        command=on_click,
-         *args, **kargs
-    )
-    
-def Label(window: tk.Tk, text: str = "", font_size = 15, *args, **kargs) -> tk.Label:
-    return tk.Label(
-        window,
-        text = text,
-        font=("Times New Roman", font_size),
-        *args, **kargs
-    )
+class Button(tk.Button):
+    def __init__(self, window: tk.Tk, text: str = "", on_click = None, *args, **kargs):
+        super().__init__(
+            window, 
+            text = text, 
+            font = ("Times New Roman", 17), 
+            bg = Color.BLACK.value, 
+            fg = Color.WHITE.value,
+            command=on_click,
+            *args, **kargs
+       )
 
-def Entry(window: tk.Tk, font_size = 15,  *args, **kargs ) -> tk.Entry:
-    return tk.Entry(
-        window,
-        font=("Times New Roman", font_size),
-        *args, **kargs
-    )
+class Label(tk.Label):
+    def __init__(self, window: tk.Tk, text: str = "", font_size = 15, *args, **kargs):
+        super().__init__(
+            window,
+            text = text,
+            font=("Times New Roman", font_size),
+            *args, **kargs
+        )
+
+class Entry(tk.Entry):
+    def __init__(self, window: tk.Tk, font_size = 15, *args, **kargs):
+        super().__init__(
+            window,
+            font=("Times New Roman", font_size),
+            *args, **kargs
+        )
